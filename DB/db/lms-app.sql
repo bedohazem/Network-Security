79f6520508ddfd99bf57efb3b997686e831f68aa
Mahmoud Ragab <104703847+MRagab2@users.noreply.github.com>
2024-04-23 19:36:14 +0200

    Update lms-app.sql

--- DB/db/lms-app.sql
+++ DB/db/lms-app.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.3
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 14, 2023 at 03:20 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 7.4.29
+-- Generation Time: Apr 23, 2024 at 07:35 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `lms-app`
 --
+CREATE DATABASE IF NOT EXISTS `lms-app` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `lms-app`;
 
 -- --------------------------------------------------------
 
@@ -30,20 +32,19 @@ SET time_zone = "+00:00";
 CREATE TABLE `courses` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `code` varchar(255) NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT 'active/in-active'
+  `code` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `courses`
 --
 
-INSERT INTO `courses` (`id`, `name`, `code`, `status`) VALUES
-(2, 'SW-2', 'IS331', 1),
-(3, 'system analysis', 'IS1234', 1),
-(11, ' computer graphics', 'IS1234', 1),
-(12, 'internet application', 'IS1235', 2),
-(13, ' database one', 'IS1235', 1);
+INSERT INTO `courses` (`id`, `name`, `code`) VALUES
+(2, 'SW-2', 'IS331'),
+(3, 'system analysis', 'IS1234'),
+(11, ' computer graphics', 'IS1234'),
+(12, 'internet application', 'IS1235'),
+(14, 'Artificial Intelligence ', 'AI 101');
 
 -- --------------------------------------------------------
 
@@ -62,7 +63,8 @@ CREATE TABLE `instructor_course` (
 --
 
 INSERT INTO `instructor_course` (`id`, `userId`, `courseId`) VALUES
-(1, 11, 2);
+(6, 18, 2),
+(7, 20, 3);
 
 -- --------------------------------------------------------
 
@@ -76,24 +78,19 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `phone` varchar(255) NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT 'active/in-active',
   `token` varchar(255) NOT NULL,
-  `role` tinyint(1) NOT NULL DEFAULT 3 COMMENT 'admin -> 1\r\ninstructor -> 2\r\nstudent -> 3'
+  `role` int(11) NOT NULL DEFAULT 3 COMMENT '-admin\r\n-instructor\r\n-student'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `phone`, `status`, `token`, `role`) VALUES
-(1, 'menna ahmed', 'menna@gamil.com', '$2b$10$IufuLcFewXqjhw1LGF2UOeYnx8i4Pl5jWueRrJx2BZBXEKKB7lc7i', '12345678', 1, '84d36022aca0d7b390a863812db214cd', 3),
-(3, 'amina nasser', 'amina@gamil.com', '$2b$10$lcwYlUZfaYNIGJTaR0vdM.TCmwR25SydFTguMuNA77youBZ8VlXfW', '12345679', 1, '658463e14744121a11554867234069b4', 1),
-(5, 'fatma ashraf', 'fatma@gamil.com', '$2b$10$NWwWAZAcgwuoHUa0W/G3t.0xB7hIr.2Wb1f0klKBzNFlgBaPfvej2', '12345679', 1, '0ccae8eec55715ed04a41a867643596a', 3),
-(6, 'admin admin', 'admin@gamil.com', '$2b$10$AGM3YsYgI0fd4DPGT.B.1uIWoO3jZJWA.H66ggz1WTuj.fJ3U/Pja', '12345679', 1, '43fbf621e4ea5aaf5d1717e1cac5a665', 1),
-(8, 'student student', 'student@gamil.com', '$2b$10$h0EiENe4.JfiQuKnEZY8FOlSD9umSoIuwCLoF0gKCOHBHVPhh0YRu', '12345679', 1, 'ef4f81ec92df4665cb667d8beb03cd6d', 3),
-(10, 'student student', 'student@gamil.com', '$2b$10$T0IPimDxVDUg0zxD5NpNJ.gjMADCfys5dL8FUQ.SriZNC.T2OXF0i', '12345679', 1, '3b0df2f06b02b6cc8d884b978f430b3c', 3),
-(11, 'instructor', 'instructor@gamil.com', '$2b$10$bgdX.N6H0lsUDhMwM3vYROgj3MWpy7wcQVq59whHFRrl0wdo04QuC', '12345679', 1, 'd1a838af6175e69d87ca58f74cbc815b', 2),
-(12, 'ahmed mahmoud', 'ahmed@gamil.com', '$2b$10$kpiHsijsCdqpnVX2yxPY3ualsY41Tg7aRSpKNCoQvc42CDebOZUCu', '12345679', 1, '11e6dce9cb1b0be27d4ae3aa600c3231', 2);
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `phone`, `token`, `role`) VALUES
+(18, 'test', 'test@gmail.com', '$2b$10$KE7HM7iNYckm1hRO67umoOy8zZUu4HoF9xZUNE8eaC/zNZKB5i1RO', '12345678', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoidGVzdCIsImVtYWlsIjoidGVzdEBnbWFpbC5jb20iLCJzdGF0dXMiOjEsInJvbGUiOiJpbnN0cnVjdG9yIiwiaWF0IjoxNzEzODkwNDM5fQ.JlosrtTnc8xBPZixoZgwPSlWnh6m4r1B3IGAOH8CLp4', 2),
+(19, 'admitest', 'admintest@gmail.com', '$2b$10$rdXCe3mtwv9p5/bYF22Z0.fo368hE6M9/dQ4ndh2DWwcWvMtZe2P6', '12345678', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiYWRtaXRlc3QiLCJlbWFpbCI6ImFkbWludGVzdEBnbWFpbC5jb20iLCJzdGF0dXMiOjEsInJvbGUiOiJhZG1pbiIsImlhdCI6MTcxMzg5MDQ4Mn0.C88Y1AfT_4vqnP_eg9V5AW9t_WlDb7aB4yIjjMWgaTA', 1),
+(20, 'test2', 'test2@gmail.com', '$2b$10$ZY5KYp4rYw8jRW/tOiVNaOD1BT67w9/oxwNBC9aHy.w4SJW.XSaIq', '12345678', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoidHNldDIiLCJlbWFpbCI6InRzZXQyQGdtYWlsLmNvbSIsInN0YXR1cyI6MSwicm9sZSI6Imluc3RydWN0b3IiLCJpYXQiOjE3MTM4OTA1NDh9.Y8YcIk0RjF3gLkkyd5-SarLl22oig_1iM4Cj5M1o7kU', 2),
+(21, 'studentTest', 'studentTest@studentTest.com', '$2b$10$NbhYOB/TnrY2fn5ZDyfRLuroPPXLMD9UGprJ2g0H0YDgJuN/li6E.', '12345678', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoic3R1ZGVudFRlc3QiLCJlbWFpbCI6InN0dWRlbnRUZXN0QHN0dWRlbnRUZXN0LmNvbSIsInJvbGUiOiJzdHVkZW50IiwiaWF0IjoxNzEzODkyNDcxfQ.1-dhfVahwm-krSYzKFtfAoJqTO9daQo4tT0lk1Antoo', 3);
 
 -- --------------------------------------------------------
 
@@ -113,11 +110,9 @@ CREATE TABLE `user_course` (
 --
 
 INSERT INTO `user_course` (`id`, `userId`, `courseId`, `grade`) VALUES
-(5, 8, 12, 70),
-(6, 1, 13, 82),
-(7, 1, 2, 0),
-(8, 1, 3, 0),
-(9, 8, 3, 0);
+(11, 21, 2, 55),
+(12, 21, 12, 0),
+(13, 21, 3, 3);
 
 --
 -- Indexes for dumped tables
@@ -159,25 +154,25 @@ ALTER TABLE `user_course`
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `instructor_course`
 --
 ALTER TABLE `instructor_course`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `user_course`
 --
 ALTER TABLE `user_course`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Constraints for dumped tables
